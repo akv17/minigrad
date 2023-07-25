6721ccdc4405090059270ae81ada11231637ae0b
akv17 <temakep@list.ru>
2023-07-25 18:23:19 +0300

    add linear (backward is not equal to torch for large inputs i.e. 128x256)

--- minigrad/nn.py
+++ minigrad/nn.py
@@ -22,22 +22,53 @@ class Neuron:
         assert len(x) == self.size
         call_id = uid()
         linear = sum([xi * wi for xi, wi in zip(x, self._w)]) + self._b
-        linear.name = f'linear@{call_id}#{self.name}'
+        linear.name = f'linear@{call_id}@{self.name}'
         act = getattr(linear, self.activation)()
-        act.name = f'act@{call_id}#{self.name}'
+        act.name = f'act@{call_id}@{self.name}'
         return act
 
     def initialize(self, w=None, b=None):
         if w is not None:
             assert len(w) == self.size
         w = w or [random.uniform(-1.0, 1.0) for _ in range(self.size)]
-        self._w = [Node(wi, name=f'w{i}#{self.name}') for i, wi in enumerate(w)]
+        self._w = [Node(wi, name=f'w{i}@{self.name}') for i, wi in enumerate(w)]
         b = b or random.uniform(-1.0, 1.0)
-        self._b = Node(b, name=f'b#{self.name}')
+        self._b = Node(b, name=f'b@{self.name}')
 
     def parameters(self):
         return [*self._w, self._b]
 
 
 class Linear:
-    pass
+    
+    def __init__(self, size_in, size_out, activation='identity', name=None):
+        self.size_in = size_in
+        self.size_out = size_out
+        self.activation = activation
+        self.name = name or f'linear@{uid()}'
+
+        self._neurons = None
+        self.initialize()
+    
+    def __repr__(self):
+        return f'Linear(name={self.name}, size_in={self.size_in}, size_out={self.size_out}))'
+
+    def __call__(self, x):
+        assert len(x) == self.size_in
+        out = [n(x) for n in self._neurons]
+        return out
+
+    def initialize(self, neurons=None):
+        self._neurons = neurons
+        if self._neurons is None:
+            self._neurons = [
+                Neuron(
+                    size=self.size_in,
+                    activation=self.activation,
+                    name=f'neuron{i}@{self.name}'
+                )
+                for i in range(self.size_in)
+            ]
+
+    def parameters(self):
+        return [p for n in self._neurons for p in n.parameters()]
--- tests/test_nn.py
+++ tests/test_nn.py
@@ -8,11 +8,11 @@ try:
 except ImportError:
     raise ImportError('PyTorch required to run tests')
 
-from minigrad.nn import Neuron
+from minigrad.nn import Neuron, Linear
 
 
 class TestNN(unittest.TestCase):
-    FP_PRECISION = 5
+    FP_PRECISION = 3
 
     @parameterized.expand([
         (2, 'identity'),
@@ -58,10 +58,63 @@ class TestNN(unittest.TestCase):
         m_out.backward()
         m_grad = [p.grad for p in m_neuron.parameters()]
 
-        self.assertAlmostEqual(t_out.item(), m_out.data, places=self.FP_PRECISION, msg='data')
-        self.assertEqual(len(t_grad), len(m_grad), msg='size')
-        for t_v, m_v in zip(t_grad, m_grad):
-            self.assertAlmostEqual(t_v, m_v, places=self.FP_PRECISION, msg='grad')
+        self.assertAlmostEqual(t_out.item(), m_out.data, places=self.FP_PRECISION, msg='forward')
+
+        t_bwd = self._round(t_grad)
+        m_bwd = self._round(m_grad)
+        self.assertEqual(len(t_grad), len(m_grad), msg='backward_len')
+        self.assertListEqual(t_bwd, m_bwd, msg='backward_len')
+
+    @parameterized.expand([
+        (2, 1, 'identity'),
+        (1, 5, 'identity'),
+        (3, 4, 'identity'),
+        (24, 32, 'identity'),
+
+        (2, 1, 'relu'),
+        (1, 5, 'relu'),
+        (3, 4, 'relu'),
+        (24, 32, 'relu'),
+
+        (2, 1, 'sigmoid'),
+        (1, 5, 'sigmoid'),
+        (3, 4, 'sigmoid'),
+        (24, 32, 'sigmoid'),
 
 
+    ])
+    def test_linear(self, size_in, size_out, activation):
+        t_lin = torch.nn.Linear(size_in, size_out)
 
+        w = t_lin.weight.data.tolist()
+        b = t_lin.bias.data.tolist()
+        inp = [random.uniform(-1, 1) for _ in range(size_in)]
+        
+        t_inp = torch.tensor(inp)
+        t_out = t_lin(t_inp)
+        if activation != 'identity':
+            t_out = getattr(t_out, activation)()
+        t_out = sum(t_out)
+        # must reduce to scalar to enable backward.
+        t_out.backward()
+            
+        
+        m_neurons = [Neuron(size=size_in, activation=activation) for _ in range(size_out)]
+        self.assertEqual(len(m_neurons), len(w))
+        for m_n, wi, bi in zip(m_neurons, w, b):
+            m_n.initialize(w=wi, b=bi)
+        m_lin = Linear(size_in=size_in, size_out=size_out, activation=activation)
+        m_lin.initialize(m_neurons)
+        m_out = m_lin(inp)
+        m_out = sum(m_out)
+        m_out.backward()
+        
+        self.assertAlmostEqual(t_out.item(), m_out.data, places=self.FP_PRECISION, msg='forward')
+
+        t_w_bwd = self._round(t_lin.weight.grad.ravel().tolist())
+        m_w_bwd = self._round([w.grad for n in m_lin._neurons for w in n._w])
+        self.assertEqual(len(t_w_bwd), len(m_w_bwd), msg='backward_len')
+        self.assertListEqual(t_w_bwd, m_w_bwd, msg='backward')
+
+    def _round(self, x):
+        return [round(v, self.FP_PRECISION) for v in x]
