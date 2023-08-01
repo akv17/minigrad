95c0172d3502b1030808d2354ef7e1c7f094319c
akv17 <temakep@list.ru>
2023-08-01 18:23:34 +0300

    reimpl trainer

--- scalargrad/train.py
+++ scalargrad/train.py
@@ -4,37 +4,79 @@ import random
 from .nn import Softmax
 
 
-def split_train_val(x, y, val_size):
-    val_size = int(round(len(x) * val_size, 0)) if isinstance(val_size, float) else val_size
-    val_ixs = set(random.sample(range(len(x)), val_size))
-    x_train = [xi for i, xi in enumerate(x) if i not in val_ixs]
-    x_val = [xi for i, xi in enumerate(x) if i in val_ixs]
-    y_train = [yi for i, yi in enumerate(y) if i not in val_ixs]
-    y_val = [yi for i, yi in enumerate(y) if i in val_ixs]
-    return x_train, y_train, x_val, y_val
+class Trainer:
 
-
-class Dataset:
-
-    def __len__(self):
-        pass
-
-    def __getitem__(self):
-        pass
-
-
-class XYDataset(Dataset):
-
-    def __init__(self, x, y):
-        assert len(x) == len(y)
+    def __init__(
+        self,
+        x,
+        y,
+        model,
+        loss,
+        optimizer,
+        batch_size,
+        num_epochs,
+        evaluator=None,
+        logger=None,
+    ):
         self.x = x
         self.y = y
+        self.model = model
+        self.loss = loss
+        self.optimizer = optimizer
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.evaluator = evaluator
+        self.logger = logger
+        self.losses = None
+        if self.logger is None:
+            fmt = '%(asctime)s - [%(levelname)s] - %(name)s - %(message)s'
+            logger = logging.getLogger('scalargrad')
+            logger.setLevel('INFO')
+            ch = logging.StreamHandler()
+            ch.setLevel('INFO')
+            formatter = logging.Formatter(fmt)
+            ch.setFormatter(formatter)
+            logger.addHandler(ch)
+            self.logger = logger
+
     
-    def __len__(self):
-        return len(self.x)
+    def run(self):
+        self.logger.info('Training...')
+        self.losses = []
+        batch_size = self.batch_size
+        for epoch in range(self.num_epochs):
+            epoch += 1
+            for i in range(0, len(self.x), batch_size):
+                xb = self.x[i:i + batch_size]
+                yb = self.y[i:i + batch_size]
+                outputs = [self.model(xi) for xi in xb]
+                targets = yb
+                loss = self.loss(outputs, targets)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                self.losses.append(loss.data)
+            self._on_epoch_end(epoch)
     
-    def __getitem__(self, ix):
-        return self.x[ix], self.y[ix]
+    def _on_epoch_end(self, epoch):
+        loss = sum(self.losses) / len(self.losses)
+        self.logger.info(f'Epoch: {epoch}/{self.num_epochs}    Loss: {loss:.4f}')
+
+        if self.evaluator is None:
+            return
+        self.logger.info('evaluating...')
+        softmax = Softmax()
+        outputs = []
+        targets = []
+        for x, y in self._iter_batches(self.dataset_val):
+            for xi, yi in zip(x, y):
+                pred = softmax(self.model(xi))
+                pred = [p.data for p in pred]
+                outputs.append(pred)
+                targets.append(yi)
+        score = self.metric(outputs, targets)
+        self.logger.info(f'score: {score}')
+        self.optimizer.zero_grad()
 
 
 class AccuracyMetric:
@@ -51,73 +93,11 @@ class AccuracyMetric:
         return score
 
 
-class Trainer:
-
-    def __init__(
-        self,
-        dataset_train,
-        model,
-        loss,
-        optimizer,
-        batch_size,
-        num_epochs,
-        metric=None,
-        dataset_val=None,
-        logger=None,
-    ):
-        self.dataset_train = dataset_train
-        self.model = model
-        self.loss = loss
-        self.optimizer = optimizer
-        self.batch_size = batch_size
-        self.num_epochs = num_epochs
-        self.metric = metric
-        self.dataset_val = dataset_val
-        self.logger = logger
-        if self.logger is None:
-            self.logger = logging.getLogger('scalargrad')
-            self.logger.setLevel('INFO')
-            logging.basicConfig()
-    
-    def run(self):
-        losses = []
-        step = 0
-        for epoch in range(self.num_epochs):
-            epoch += 1
-            for x, y in self._iter_batches(self.dataset_train):
-                step += 1
-                outputs = [self.model(xi) for xi in x]
-                targets = y
-                loss = self.loss(outputs, targets)
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-                losses.append(loss.data)
-                loss_value = sum(losses) / len(losses)
-                self.logger.info(f'[train step] epoch: {epoch} step: {step} loss: {loss_value:.4f}')
-            self._on_epoch_end()
-    
-    def _iter_batches(self, dataset):
-        step = self.batch_size
-        for i in range(0, len(dataset), step):
-            batch = [dataset[j] for j in range(i, i + step) if j < len(dataset)]
-            x = [it[0] for it in batch]
-            y = [it[1] for it in batch]
-            yield x, y
-
-    def _on_epoch_end(self):
-        if self.metric is None or self.dataset_val is None:
-            return
-        self.logger.info('evaluating...')
-        softmax = Softmax()
-        outputs = []
-        targets = []
-        for x, y in self._iter_batches(self.dataset_val):
-            for xi, yi in zip(x, y):
-                pred = softmax(self.model(xi))
-                pred = [p.data for p in pred]
-                outputs.append(pred)
-                targets.append(yi)
-        score = self.metric(outputs, targets)
-        self.logger.info(f'score: {score}')
-        self.optimizer.zero_grad()
+def split_train_val(x, y, val_size):
+    val_size = int(round(len(x) * val_size, 0)) if isinstance(val_size, float) else val_size
+    val_ixs = set(random.sample(range(len(x)), val_size))
+    x_train = [xi for i, xi in enumerate(x) if i not in val_ixs]
+    x_val = [xi for i, xi in enumerate(x) if i in val_ixs]
+    y_train = [yi for i, yi in enumerate(y) if i not in val_ixs]
+    y_val = [yi for i, yi in enumerate(y) if i in val_ixs]
+    return x_train, y_train, x_val, y_val
